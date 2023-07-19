a933bd38dece02a877a1c78df7841616d49af5e0
javad <javadr@gmail.com>
2023-07-19 20:37:24 +0330

    (style) clean code

--- BankDataAnomalyDetection.py
+++ BankDataAnomalyDetection.py
@@ -1,23 +1,22 @@
-import matplotlib.pyplot as plt
+#!/usr/bin/env python3
+
+import re
+import time
+import warnings
+from datetime import datetime
+from itertools import combinations
+
 import numpy as np
 import pandas as pd
-from pandas.api.types import is_datetime64_any_dtype as is_datetime
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import IsolationForest
-
-from itertools import combinations
-from datetime import datetime
 from dateutil import parser
-
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
 from rich import print
 from rich_dataframe import prettify
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import LabelEncoder
 
-import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
-import time
-import re
 # set seed for reproducibility
 np.random.seed(14)
 
@@ -32,7 +31,8 @@ def timeit(func):
     def _wrap(*args, **kwargs):
         start = time.time()
         try:
-            if func.__doc__: print(func.__doc__)
+            if func.__doc__:
+                print(func.__doc__)
             return func(*args, **kwargs)
         finally:
             print(f"Done in {timeSince(start)}.\n")
@@ -41,7 +41,7 @@ def timeit(func):
 
 
 def highVar(df: pd.DataFrame, hvPercent=.7) -> dict:
-    """ 
+    """
     Return a list containing the columns with high varibility and thier correspondig uniqueness values
     """
     hv = {}  # list of columns with high variability
@@ -53,11 +53,11 @@ def highVar(df: pd.DataFrame, hvPercent=.7) -> dict:
 
 
 def colIsDate(series: pd.Series, ntries: int = 10) -> bool:
-    """ 
+    """
     Checks #ntries elements of a specified column and returns True if it contains datetime element.
     """
     #courtesy to https://stackoverflow.com/questions/33204500/pandas-automatically-detect-date-columns-at-run-time
-    if series.dtype != 'object':
+    if series.dtype != "object":
         return False
     elif is_datetime(series.dtype):
         return True
@@ -70,8 +70,10 @@ def colIsDate(series: pd.Series, ntries: int = 10) -> bool:
 
     for val in list(vals):
         try:
-            if isinstance(val, (int, float)): continue
-            elif isinstance(val, datetime): return True
+            if isinstance(val, (int, float)):
+                continue
+            elif isinstance(val, datetime):
+                return True
             parser.parse(val)
             return True
         except ValueError:
@@ -85,7 +87,7 @@ def findDateCol(df: pd.DataFrame) -> list:
     Returns columns' list of datetime type in a dataframe
     """
     dateCols = [c for c in df.columns if is_datetime(df[c])]
-    cols = df.columns.values[df.dtypes.values == 'object']
+    cols = df.columns.values[df.dtypes.values == "object"]
     dateCols.extend([c for c in cols if colIsDate(df[c], ntries=10)])
 
     return dateCols
@@ -101,12 +103,14 @@ def task1(
     Normalizing numeric attributes
     Filling empty attributes
     Erasing surrounding spaces and makes all strings lowre-case
-    Deleting columns of high variablity"""
+    Deleting columns of high variablity
+    """
+
     df = df.copy()
 
     # normalizing numerical fields & filling the empty deposit/withdrawal with 0
     for col in numericals:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
+        df[col] = pd.to_numeric(df[col], errors="coerce")
         df[col] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
         # uncomment the below if you want z-score normalization instead min-max normalization
         #df[col] = (df[col]-df[col].mean()) / df[col].std()
@@ -136,27 +140,27 @@ def task2(df: pd.DataFrame) -> pd.DataFrame:
     dateCandids = findDateCol(df)
 
     attr = [
-        'Year', 'Month', 'Week', 'Day', 'Dayofweek', 'Dayofyear',
-        'Is_month_end', 'Is_month_start', 'Is_quarter_end', 'Is_quarter_start',
-        'Is_year_end', 'Is_year_start'
+        "Year", "Month", "Week", "Day", "Dayofweek", "Dayofyear",
+        "Is_month_end", "Is_month_start", "Is_quarter_end", "Is_quarter_start",
+        "Is_year_end", "Is_year_start"
     ]
     regex = "([0-9]+[-/][0-9]+[-/][0-9]+)"
 
     for dc in dateCandids:
         # extracting text part of the date filed
         df[f"{dc} text"] = df[dc].apply(
-            lambda x: ' '.join(re.findall(r'([a-zA-Z_\- ]+)', x)).strip()
-            if isinstance(x, str) else 'exact')
-        df[f"{dc} text"] = df[f"{dc} text"].astype('category').cat.as_ordered()
+            lambda x: " ".join(re.findall(r"([a-zA-Z_\- ]+)", x)).strip()
+            if isinstance(x, str) else "exact")
+        df[f"{dc} text"] = df[f"{dc} text"].astype("category").cat.as_ordered()
         df[dc] = df[dc].apply(lambda x: re.findall(regex, x)[0]
                               if isinstance(x, str) else x)
         # converting to date time type
-        df[dc] = pd.to_datetime(df[dc], errors='coerce', dayfirst=True)
+        df[dc] = pd.to_datetime(df[dc], errors="coerce", dayfirst=True)
         # adding more attributes from data fields
         for n in attr:
             df[f"{dc} {n}"] = getattr(df[dc].dt, n.lower())
             df[f"{dc} {n}"] = df[f"{dc} {n}"].astype(
-                'category').cat.as_ordered()
+                "category").cat.as_ordered()
 
     return df
 
@@ -174,14 +178,14 @@ def task3(
 
     for nc in numericals:
         text = f"{nc} text"
-        df[text] = df[nc].apply(lambda x: ' '.join(
-            re.findall(r'([^0-9]+)', x.replace(',', ''))).strip()
-                                if isinstance(x, str) else 'exact')
+        df[text] = df[nc].apply(lambda x: " ".join(
+            re.findall(r"([^0-9]+)", x.replace(",", ""))).strip()
+                                if isinstance(x, str) else "exact")
         df[nc] = df[nc].apply(
             lambda x: np.nan if pd.isnull(x) or x == [] else pd.
-            to_numeric(re.findall(regex, x.replace(',', ''))[0]) if isinstance(
-                x, str) and len(re.findall(regex, x.replace(',', ''))) else x)
-        df[text] = df[text].astype('category').cat.as_ordered()
+            to_numeric(re.findall(regex, x.replace(",", ""))[0]) if isinstance(
+                x, str) and len(re.findall(regex, x.replace(",", ""))) else x)
+        df[text] = df[text].astype("category").cat.as_ordered()
         df[nc] = df[nc].fillna(0)
     return df
 
@@ -189,8 +193,8 @@ def task3(
 @timeit
 def task4(df: pd.DataFrame,
           columns: list = [
-              'Account No', 'DATE', 'TRANSACTION DETAILS', 'VALUE DATE',
-              'WITHDRAWAL AMT', 'DEPOSIT AMT', 'BALANCE AMT'
+              "Account No", "DATE", "TRANSACTION DETAILS", "VALUE DATE",
+              "WITHDRAWAL AMT", "DEPOSIT AMT", "BALANCE AMT"
           ],
           dupPercent: float = .7) -> pd.DataFrame:
     """Data cleaning ...
@@ -207,11 +211,11 @@ def task4(df: pd.DataFrame,
 
     df = df.copy()
 
-    catCols = [c for c in df.columns if df[c].dtype.name in 'category']
+    catCols = [c for c in df.columns if df[c].dtype.name in "category"]
     # trimming surrounding spaces of data strings and making them in lower case
     df = df.applymap(lambda x: x.lower().strip() if isinstance(x, str) else x)
     for col in catCols:
-        df[col] = df[col].astype('category').cat.as_ordered()
+        df[col] = df[col].astype("category").cat.as_ordered()
 
     for dc in colList:
         df.drop_duplicates(subset=dc, inplace=True)
@@ -233,7 +237,7 @@ def dataSanitization(df: pd.DataFrame) -> pd.DataFrame:
 
 print("Reading input file ...")
 start = time.time()
-df = pd.read_excel('bank.xlsx')
+df = pd.read_excel("bank.xlsx")
 print(f"Read [bold]{len(df)}[/bold] lines in {timeSince(start)}.\n")
 #df= df.rename(columns=str.lower)
 sdf = dataSanitization(df)
@@ -249,22 +253,22 @@ iforest = IsolationForest(contamination=.01,
 start = time.time()
 features = [
     c for c in sdf.columns
-    if sdf[c].dtype.name in ('float64', 'int64', 'category')
+    if sdf[c].dtype.name in ("float64", "int64", "category")
 ]
 
 for c in features:
-    if sdf[c].dtype.name == 'category':
+    if sdf[c].dtype.name == "category":
         le.fit(sdf[c])
         sdf[c] = le.transform(sdf[c])
 
 iforest.fit(sdf[features])
-sdf['score'] = iforest.decision_function(sdf[features])
-sdf['anomaly'] = iforest.predict(sdf[features])
+sdf["score"] = iforest.decision_function(sdf[features])
+sdf["anomaly"] = iforest.predict(sdf[features])
 print(
     f"Anomaly Detection takes [bold red]{timeSince(start)}[/bold red] with Isolataion Forest Algorithm.\n"
 )
 
-prettify(sdf.iloc[np.where(sdf['anomaly'] == -1, True, False)][[
+prettify(sdf.iloc[np.where(sdf["anomaly"] == -1, True, False)][[
     "Account No", "DATE", "TRANSACTION DETAILS", "VALUE DATE",
     "WITHDRAWAL AMT", "DEPOSIT AMT", "BALANCE AMT"
 ]])
